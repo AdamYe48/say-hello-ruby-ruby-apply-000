8c5109923f8e97b71e21c9f4e482e711522c0720
ADAM YE <adamye48@gmail.com>
2020-07-20 19:10:01 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
-def say_hello(name, ruby programmer)
+def say_hello(name)
   puts "Hello Kent Beck!", "Hello Ruby Programmer"
 #  puts "Hello Ruby Programmer!"
 end
